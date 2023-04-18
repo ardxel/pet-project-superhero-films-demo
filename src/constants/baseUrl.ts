a32e325acbb4d025e7fa6e9c7c3c899ac58972f3
Vasily Bobnev <ardxel@gmail.com>
2023-04-18 11:05:44 +0300

    Update baseUrl.ts

--- src/constants/baseUrl.ts
+++ src/constants/baseUrl.ts
@@ -1,8 +1,3 @@
-// if you want to test the application on a smartphone or any other device
-// add your private ip address
-// for example http://192.168.0.103:5000
-const myUrl = 'http://192.168.0.103:5000';
-
-const BASE_URL = myUrl || 'http://localhost:5000';
+const BASE_URL = 'https://superhero-films-express.vercel.app/';
 
 export default BASE_URL;
