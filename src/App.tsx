9e4b4120e8f8f67ddbbb6e402899dd53de3af0b1
Vasily Bobnev <ardxel@gmail.com>
2023-04-18 13:07:50 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,19 @@ import React, { Suspense } from 'react';
 import '@styles/main.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ScrollToTop from '@common/scroll-to-top/ScrollToTop';
+import AppFallback from '@common/fallback/AppFallback';
 import Header from '@components/header/Header';
 import Footer from '@components/footer/Footer';
 import { Provider } from 'react-redux';
 import store from '@reduxproj/store';
 import appRoutes from './AppRoutes';
-import ReactLoading from 'react-loading';
 
 const App = () => (
   <BrowserRouter>
     <ScrollToTop />
     <Provider store={store}>
       <Header />
-      <Suspense fallback={<ReactLoading type='balls' />}>
+      <Suspense fallback={<AppFallback size={200}/>}>
         <Routes>
           {appRoutes.map(({ path, Element, index }, i) => {
             return <Route key={i} index={index} path={path}
